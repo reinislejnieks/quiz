fe2b3e2cae79cc8e5d0441ce6d0577bf97815e2e
rlejnieks <rlejnieks@gmail.com>
2018-08-28 13:47:56 +0300

    Created new tests directory and initial unit test for the new code

---
+++ tests/BaseDbRepositoryTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Quiz\Tests;
+
+class BaseDbRepositoryTest extends \PHPUnit\Framework\TestCase
+{
+    public function testTrue()
+    {
+        self::assertTrue(true);
+    }
+}
+
+
